c5ac7dce566fbaa369a1531cae2b851ddc0a330f
Deepak Aggarwal <deepak@zefi.io>
2023-02-20 14:23:06 +0530

    add: plonk proving system support

--- src/index.ts
+++ src/index.ts
@@ -4,37 +4,34 @@ import log = require('log');
 
 require("log-node")();
 
-
-const main = async () => {
-    const c = new CircomJS()
-
-    const circuit = await c.getCircuit("mul")
+async function runPipeline(c: CircomJS, cId: string, inp: any) {
+    const circuit = await c.getCircuit(cId)
     await circuit.compile()
     await circuit.genZKey()
-    const p = await circuit.genProof({x:3, y:2}) as ZK_PROOF
+    const w = await circuit.calculateWitness(inp)
+    console.log(w)
+    const p = await circuit.genProof(inp) as ZK_PROOF
     log.info("proof, circuit:%s\n", circuit._circuitConfig.cktName, p)
-
-    await  circuit.genVKey()
+    await circuit.genVKey()
     const verificationRes = await circuit.verifyProof(p)
     console.log(verificationRes)
 
-    const w = await circuit.calculateWitness({x: 3, y: 2})
-    console.log(w)
+}
+
+const main = async () => {
+    const c = new CircomJS()
+    await runPipeline(c, "mul", {x: 3, y: 2})                       // Groth16
+    await runPipeline(c, "circ1000constraints", {"a": 3, "b": 11})  // Plonk
 
 
     // const address = await circuit._deploySmartContractVerifier("")
     // console.log(address)
-
     // try {
     //     await circuit.checkConstraints(w)
     //     console.log("constraint checking success")
     // } catch (err) {
     //     console.log("constraint checking unsuccessful")
     // }
-
-
-
-
     process.exit(0)
 }
 main()
