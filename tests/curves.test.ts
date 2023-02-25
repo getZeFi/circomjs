41a838e0f4faa3c62dd92e40cca04bad5c3fa363
Shivam Saxena <shivamsaxena1243@gmail.com>
2023-02-25 14:56:57 +0530

    Removed curves.test.ts as it was not needed

--- tests/curves.test.ts
+++
@@ -1,9 +0,0 @@
-import { getCurveFromName } from "../src/utils/curves";
-
-describe("Curves test" ,() =>{
-    it("should return curve from name", async () => {
-        const curve = await getCurveFromName("bn128");
-        
-        expect(typeof curve).toBe("object");
-    })
-});
